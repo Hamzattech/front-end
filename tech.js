ff24bfbfc431048aaabb04b77bcfe329c7902bea
Ridwan <ridwanulllah@gmail.com>
2020-08-18 10:25:05 +0100

    layout

--- tech.js
+++ tech.js
@@ -1,14 +1,14 @@
 const tech = [
     {
         id:"1",
-        img:"C:/Users/Ridwan/Desktop/technologiesProject/Images/html1.png",
+        img:"Images/html1.png",
         name:"Html",
         title:"Structure of the web",
         summary:"HTML (or Hypertext Markup Language)  is a computer language designed to create websites that later can be explored by anybody who accesses the Internet. HTML is normally employed to structure a web document. It defines such elements as headlines or paragraphs and enables embedding images, video, and other media.",
     },
     {
         id:"1",
-        img:"C:/Users/Ridwan/Desktop/technologiesProject/Images/css1.png",
+        img:"Images/css1.png",
         name:"Css",
         title:"Beautifies the web",
         summary:"CSS (or Cascading Style Sheets) is a style sheet language. It’s applied to define how HTML elements are supposed to be presented on a webpage in terms of design, layout, and variations for diverse devices with different screen sizes. CSS masters the layout of numerous different web pages at a time."
@@ -16,7 +16,7 @@ const tech = [
     },
     {
         id:"1",
-        img:"C:/Users/Ridwan/Desktop/technologiesProject/Images/stuff.png",
+        img:"Images/stuff.png",
         name:"Javascript",
         title:"Gets the web working",
         summary:"JavaScript (JS) is one of the most popular scripting languages. It’s mostly renowned for providing a full stack of technologies for both front-end and back-end development. As we are talking about the first one, it’s applied to make web pages dynamic."
@@ -24,7 +24,7 @@ const tech = [
     },
     {
         id:"1",
-        img:"C:/Users/Ridwan/Desktop/technologiesProject/Images/jquery1.png",
+        img:"Images/jquery1.png",
         name:"jQuery",
         title:"Javascript Library",
         summary:"jQuery, in turn, is aimed at controlling HTML documents. It has a simple API to control events and design animation in browsers. Aside from that, jQuery is applied to manipulate the Document Object Model (DOM) and serves as a plugin development tool as well. It also comes with a lighter cross-browser library."
@@ -32,7 +32,7 @@ const tech = [
     },
     {
         id:"1",
-        img:"C:/Users/Ridwan/Desktop/technologiesProject/Images/bootstrap1.png",
+        img:"Images/bootstrap1.png",
         name:"Bootstrap",
         title:"Write less css code",
         summary:"A CSS framework that has a set of default CSS and HTML files. It extends a front-end developer’s capabilities for website design. In addition to assisting while building a responsive design, It also present distinct and symmetric layouts, saving developers from writing code from ground zero at every occasion."
@@ -40,7 +40,7 @@ const tech = [
     },
     {
         id:"1",
-        img:"C:/Users/Ridwan/Desktop/technologiesProject/Images/react1.png",
+        img:"Images/react1.png",
         name:"React",
         title:"Javascript framework",
         summary:"React is an open-source library for building dynamic user interfaces, born and bred by Facebook. The framework is applied to create web-applications with multiple dynamic components. It relies on JavaScript and JSX, a Facebook PHP language extension. React enables building reusable HTML elements for the front end."
